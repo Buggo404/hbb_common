b51aa22c3e70c20473b30ae4fa87dd9b5f288438
Buggo404 <3838119820@qq.com>
2025-07-14 20:36:06 +0800

    修改服务器和key

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["43.136.132.136"];
-pub const RS_PUB_KEY: &str = "L3NaCwacy6U2yhY37i3KU5Dj7SLSHjgP4VclDXF4GDk=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["192.140.176.128"];
+pub const RS_PUB_KEY: &str = "Oq9Yql5GbZ42Q7nuYyMhjmpTwCemEyaupXO1V85FzpU=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
